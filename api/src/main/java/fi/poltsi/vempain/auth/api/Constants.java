b51a908a88efb960d5ce2154dfe5259557a95807
Paul-Erik Törrönen <poltsi@poltsi.fi>
2025-08-23 13:00:40 +0300

    Update Gradle to 9.0.0 Remove unused spring-data-jpa and spring-boot-starter-data-jpa Replace spring-boot-starter-web with spring-context, spring-web andjakarta.persistence-api Make the main paths in Constants consistent
    Signed-off-by: Paul-Erik Törrönen <poltsi@poltsi.fi>

--- api/src/main/java/fi/poltsi/vempain/auth/api/Constants.java
+++ api/src/main/java/fi/poltsi/vempain/auth/api/Constants.java
@@ -3,7 +3,7 @@ package fi.poltsi.vempain.auth.api;
 public class Constants {
 	public static final Long ADMIN_ID = 1L;
 	public static final String REST_CONTENT_PREFIX = "/content-management";
-	public static final String REST_FILE_PREFIX = REST_CONTENT_PREFIX + "/file";
+	public static final String REST_FILE_PREFIX = "/file-management";
 	public static final String REST_SCHEDULE_PREFIX = "/schedule-management";
 	public static final String REST_ADMIN_PREFIX = "/admin-management";
 	public static final String LOGIN_PATH = "/login";
